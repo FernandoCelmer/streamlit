753d0b920a23eed6ab0c12e8164e6f8d3ea8997d
FernandoCelmer <email@fernandocelmer.com>
2025-07-15 21:00:36 -0300

    Updated package name

--- lib/setup.py
+++ lib/setup.py
@@ -106,7 +106,7 @@ else:
     long_description = ""
 
 setup(
-    name="streamlit",
+    name="streamlit-in-fury",
     version=VERSION,
     description="A faster way to build and share data apps",
     long_description=long_description,
